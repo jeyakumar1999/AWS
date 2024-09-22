2dc3a830bc258b6c1d71898912c2a709e56d3a68
Jeyakumar_Techie <40323393+jaya32097@users.noreply.github.com>
2024-09-22 20:32:29 +0530

    Update touch.sh

--- touch.sh
+++ touch.sh
@@ -1,2 +1,12 @@
-touch jk.txt
-ls -l 
+#!/bin/bash
+sudo su
+sudo apt update
+sudo apt install nfs-common
+cd /home/ubuntu
+mkdir efs
+chmod -R 777 efs
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport 10.0.0.188:/ efs
+cd /home/ubuntu/efs/
+ls -l -R >>/home/ubuntu/movies.txt 
+cd /home/ubuntu/
+ls -l
