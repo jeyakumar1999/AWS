0c226bbfa88fb138d269416bce5343e8afec59c0
Jeyakumar_Techie <40323393+jaya32097@users.noreply.github.com>
2024-09-23 11:43:57 +0000

    Update touch.sh

--- touch.sh
+++ touch.sh
@@ -5,8 +5,15 @@ sudo apt install nfs-common -y
 cd /home/ubuntu
 mkdir efs
 chmod -R 777 efs
-sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport 10.0.0.188:/ efs
-cd /home/ubuntu/efs/
-ls -l -R >>/home/ubuntu/movies.txt 
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport 10.0.0.51:/ efs
+cd /home/
+mkdir efs
+chmod -R 777 efs
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport 10.0.2.182:/ efs
+cd /home/ubuntu/efs/WebSeries/XX (2023) S01
+cp www.1TamilMV.tf\ -\ XX\ S01E0* /home/efs/
+cd /home/efs/
+ls -l -R >>/home/efs/movies.txt
+cat movies.txt
 cd /home/ubuntu/
 ls -l
