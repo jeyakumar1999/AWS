b08e4990fb4c0b67e0083ff7b4b3e8992d9dab60
Jeyakumar_Techie <40323393+jeyakumar1999@users.noreply.github.com>
2026-02-24 00:01:02 +0530

    Update s3.bat

--- s3.bat
+++ s3.bat
@@ -1,4 +1,12 @@
 @echo off
 echo Starting Sync to S3...
 aws s3 sync "E:\Cloud-main" s3://code1996 --profile ALPHA
+
+echo Sync Complete!
+
+
+@echo off
+echo Starting Sync to S3...
+aws s3 sync s3://code1996 --profile ALPHA "D:\Shell"
+
 echo Sync Complete!
